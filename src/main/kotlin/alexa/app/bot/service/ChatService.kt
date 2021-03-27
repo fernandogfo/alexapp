33eb0ffc83900f82af0c8a5ad113d3fc2e1b01c5
Fernando Oliveira <fernando.oliveira@solutis.com.br>
2021-03-27 09:22:35 -0300

    Incluindo endpoint para ativar ou desativar apenas emotes no chat

--- src/main/kotlin/alexa/app/bot/service/ChatService.kt
+++ src/main/kotlin/alexa/app/bot/service/ChatService.kt
@@ -1,6 +1,7 @@
 package alexa.app.bot.service
 
 import alexa.app.config.BotConfig
+import alexa.app.model.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -13,7 +14,11 @@ class ChatService @Autowired constructor(
         if (!botConfig.twitchChat().isChannelJoined(channel)) {
             botConfig.twitchChat().joinChannel(channel)
         }
+
         botConfig.twitchChat().sendMessage(channel, message)
     }
 
+    fun onlyEmotes(user: User, emote: Boolean){
+        botConfig.twitchChat().setEmoteOnly(user.twitchUserLogin, emote)
+    }
 }
