3a967fc693df37d06067840f25e44406f2f87ef3
Fernando Oliveira <fernando.oliveira@solutis.com.br>
2021-03-31 20:12:01 -0300

    xD

--- src/main/kotlin/alexa/app/controller/AuthController.kt
+++ src/main/kotlin/alexa/app/controller/AuthController.kt
@@ -4,10 +4,7 @@ package alexa.app.controller
 import alexa.app.service.AuthService
 import alexa.app.utils.JwtUtil
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.logging.Logger
 
 
@@ -17,11 +14,11 @@ class AuthController @Autowired constructor(private val authService: AuthService
                                             private val jwtUtil: JwtUtil) {
     val logger: Logger = Logger.getLogger(AuthController::class.java.name)
 
-    @GetMapping
-    fun getAuth(@RequestParam("code") code: String, @RequestParam("Authorization") jwt: String): String {
-        logger.info("GET -> v1/auth")
-        logger.info("Registrando usuário de code: ${code} e alexa id: ${jwtUtil.decode(jwt)}")
-        return authService.registerUser(code, jwtUtil.decode(jwt)).toString()
-    }
+//    @GetMapping
+//    fun getAuth(@RequestParam("code") code: String, @RequestHeader("Authorization") jwt: String): String {
+//        logger.info("GET -> v1/auth")
+//        logger.info("Registrando usuário de code: ${code} e alexa id: ${jwtUtil.decode(jwt)}")
+//        return authService.registerUser(code, jwtUtil.decode(jwt)).toString()
+//    }
 
 }
